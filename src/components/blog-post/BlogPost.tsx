0133d09db3685de1327b07280056618727a25145
Evan Stern <evanstern@machineservant.com>
2023-02-12 06:34:53 -0500

    Fix blog post title h1 tags

--- src/components/blog-post/BlogPost.tsx
+++ src/components/blog-post/BlogPost.tsx
@@ -46,9 +46,9 @@ export const BlogPost: React.FC<PageProps<Queries.BlogPostPageQuery>> = ({
       content={
         <>
           <div className="container mx-auto flex h-full items-end justify-center py-12">
-            <div className="z-20 text-center text-4xl font-bold uppercase text-white">
+            <h1 className="z-20 text-center text-4xl font-bold uppercase text-white">
               {data?.mdx?.frontmatter?.title}
-            </div>
+            </h1>
           </div>
           <div className="absolute top-0 left-0 z-10 h-full w-full bg-gray-900 opacity-30" />
         </>
